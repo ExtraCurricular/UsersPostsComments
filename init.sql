cab13981013d03d5fb55777ae31f310f2bb49a65
ExtraCurricular <ExtraCurricular@github.com>
2018-03-23 10:04:01 +0200

    init sql

--- init.sql
+++ init.sql
@@ -20,24 +20,24 @@ INSERT INTO users (id,username, email) VALUES (4, "John3", "john3@john.com");
 
 CREATE TABLE posts (
 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-userId BIGINT(20),
+user_id BIGINT(20),
 title VARCHAR(50),
 body VARCHAR(500)
 )  ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-INSERT INTO posts (id, userId, title, body) VALUES (1, 1, "first title", "first post");
-INSERT INTO posts (id, userId, title, body) VALUES (2, 2, "second title", "second post");
-INSERT INTO posts (id, userId, title, body) VALUES (3, 3, "third title", "third post");
-INSERT INTO posts (id, userId, title, body) VALUES (4, 3, "fourth title", "fourth post");
+INSERT INTO posts (id, user_id, title, body) VALUES (1, 1, "first title", "first post");
+INSERT INTO posts (id, user_id, title, body) VALUES (2, 2, "second title", "second post");
+INSERT INTO posts (id, user_id, title, body) VALUES (3, 3, "third title", "third post");
+INSERT INTO posts (id, user_id, title, body) VALUES (4, 3, "fourth title", "fourth post");
 
 CREATE TABLE comments (
 id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-userId BIGINT(20),
-postId BIGINT(20),
+user_id BIGINT(20),
+post_id BIGINT(20),
 body VARCHAR(500)
 )  ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
-INSERT INTO comments (id, userId, postId, body) VALUES (1, 1, 1, "first comment on the first post!");
-INSERT INTO comments (id, userId, postId, body) VALUES (2, 1, 1, "second comment on the first post!");
-INSERT INTO comments (id, userId, postId, body) VALUES (3, 1, 1, "third comment on the first post!");
-INSERT INTO comments (id, userId, postId, body) VALUES (4, 1, 1, "fourth comment on the first post!");
+INSERT INTO comments (id, user_id, post_id, body) VALUES (1, 1, 1, "first comment on the first post!");
+INSERT INTO comments (id, user_id, post_id, body) VALUES (2, 1, 1, "second comment on the first post!");
+INSERT INTO comments (id, user_id, post_id, body) VALUES (3, 1, 1, "third comment on the first post!");
+INSERT INTO comments (id, user_id, post_id, body) VALUES (4, 1, 1, "fourth comment on the first post!");
